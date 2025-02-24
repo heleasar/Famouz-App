63000d389d8c7a23beed4a806636bc020f576e6c
Hugo Eleasar Benavides <86984765+heleasar@users.noreply.github.com>
2025-02-24 15:26:44 -0600

    Add files via upload

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,1 +1,18 @@
+import { GalleryVerticalEnd } from "lucide-react";
+import { SignUpForm } from "@/components/sign-up";
 
+export default function LoginPage() {
+  return (
+    <div className="login-page flex min-h-screen flex-col items-center justify-center gap-6 animated-bg text-white p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          void-app
+        </a>
+        <SignUpForm />
+      </div>
+    </div>
+  );
+}
